fb66ce22a497bfd7483d9d97ebcb337aa4317eaa
tinbeta <tinbeta3030@gmail.com>
2026-04-06 16:10:49 +0700

    feat: import legacy ESC Base UI and content into escbase-next

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,10 +1,2 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './styles.css'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+console.log('ESCBase Next is serving the legacy ESC Base site from /public')
